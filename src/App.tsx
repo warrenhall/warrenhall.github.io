3720bdb7177dfec39c1d0e076fb3affb0a33736b
Warren Hall <warrenkhall@gmail.com>
2026-06-21 13:48:34 +0200

    Add routing shell with fade transition and page stubs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,25 @@
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+
+function AnimatedRoutes() {
+  const location = useLocation()
+  return (
+    <main key={location.pathname} className="animate-fade-in">
+      <Routes location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </main>
+  )
+}
+
 export default function App() {
-  return <div className="p-8">Scaffolding OK</div>
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <AnimatedRoutes />
+    </BrowserRouter>
+  )
 }
